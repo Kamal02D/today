a68a6fa5625c6400f3dbf40e349c9fa3bc1906a2
KAMAL DOUMA <96614260+Kamal02D@users.noreply.github.com>
2025-04-10 09:19:27 +0100

    fix

---
+++ ToDoeyy copy/ToDoeyyTests/ToDoeyyTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoeyyTests.swift
+//  ToDoeyyTests
+//
+//  Created by kamal on 9/4/2025.
+//
+
+import Testing
+@testable import ToDoeyy
+
+struct ToDoeyyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
